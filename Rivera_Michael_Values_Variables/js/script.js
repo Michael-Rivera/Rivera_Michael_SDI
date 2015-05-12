be0b81898a357e1c265fa77a4e4f1c916875a7a7
Michael-Rivera <mtrivera@fullsail.edu>
2015-05-12 07:45:49 -0600

    Learned about single quotes and \
    Learned about single quotes and \.

--- Rivera_Michael_Values_Variables/js/script.js
+++ Rivera_Michael_Values_Variables/js/script.js
@@ -5,3 +5,9 @@ age = 28; // Definition
 // storage container = value
 
 var yearOfBirth = 1986; // Declaring and defining on the same line
+
+var isStudent = false; // Boolean Variable
+
+var firstName = "Michael";
+
+var phrase = 'I don\'t know \n You\'ll have to speak with the professor'
