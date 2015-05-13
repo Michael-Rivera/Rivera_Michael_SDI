4be7a595bb4fef85a4ab2d3700faaed86bf8ce5f
Michael-Rivera <mtrivera@fullsail.edu>
2015-05-13 00:45:15 -0600

    Order of operations
    used parenthesis to calculate the addition and subtraction before
    multiplication and division

--- Rivera_Michael_Expressions/js/script.js
+++ Rivera_Michael_Expressions/js/script.js
@@ -1,8 +1,16 @@
 /**
  * Created by michaelrivera1 on 5/12/15.
  */
-// arithmetic Operators
-// are of a triangle is half the width times the height
+// Order of Operations
 
-var remainder = 32%2;
-console.log (remainder);
+var Quiz1 = 87;
+var Quiz2 = 100;
+var Quiz3 = 60;
+var Quiz4 = 80;
+ // The average is the sum of the items divided by the number of items
+var average = (Quiz1+Quiz2+Quiz3+Quiz4)/4;
+//console.log (average);
+var length = 7;
+var width = 6;
+var perimiter = length * 2 + width * 2;
+console.log(perimiter);
