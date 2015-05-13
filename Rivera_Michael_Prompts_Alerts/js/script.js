1691537ceb89ce737db5a883fd2e390a59df0064
Michael-Rivera <mtrivera@fullsail.edu>
2015-05-13 02:59:21 -0600

    Used alerts to provide the user with information
    Used alerts to provide the user with information

--- Rivera_Michael_Prompts_Alerts/js/script.js
+++ Rivera_Michael_Prompts_Alerts/js/script.js
@@ -2,4 +2,6 @@
 var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width.");
 var height = prompt("Please enter the height.");
 var area = width* height
-console.log(area);
+//console.log(area);
+var result = "The area of your rectangle is "+ area + " square feet";
+alert (result);
