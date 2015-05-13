a9d223f2d142b43940837ff3e09857d7a7cc218d
Michael-Rivera <mtrivera@fullsail.edu>
2015-05-12 23:31:46 -0600

    Created Fahrenheit Converter and Expressions
    Created Temperature converter from book example. It didn’t work.
    Created Expressions assignment project. I keep getting  warnings in the
    console that I do not understand but the variable of 2 is being
    displayed within the console.

---
+++ Rivera_Michael_Expressions/js/script.js
@@ -0,0 +1,7 @@
+/**
+ * Created by michaelrivera1 on 5/12/15.
+ */
+// Expressions
+
+var a = 2;
+console.log (a);
---
+++ temperature_converter/html/js/script.js
@@ -0,0 +1,4 @@
+/**
+ * Created by michaelrivera1 on 5/12/15.
+ */
+prompt (Enter Degrees in Fahrenheit);
