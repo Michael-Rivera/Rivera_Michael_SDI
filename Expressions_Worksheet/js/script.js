976abb44ce83fed4f1ff1d2aab191fceb8e28ca9
Michael-Rivera <mtrivera@fullsail.edu>
2015-05-13 03:58:18 -0600

    added comments
    added comments

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -1,8 +1,9 @@
 // Michael Rivera Expressions Worksheet
 // How old is Sparky in Dog Years based on his actual age?
 
-var sparkysAge =4;
-var dogYears =7;
-var result = sparkysAge * 7 ;
+var sparkysAge =4;  // Sparkys age in human years
+var dogYears =7;  // Sparkys age in dog years
+var result = sparkysAge * 7 ; // The result is Sparkys age in human years times dog years
 
-console.log ("Sparky is "+ sparkysAge + " human years old which is " + result + " in dog years."); 
+console.log ("Sparky is "+ sparkysAge + " human years old which is " + result + " in dog years.");
+// To get the console to print the desired result I had to add strings around the variables in the console log.
