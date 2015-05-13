a4d2b719df1299e324725f6d1c77d38756a12f93
Michael-Rivera <mtrivera@fullsail.edu>
2015-05-13 02:24:50 -0600

    Learned about assignment operators
    Learned about assignment operators

--- Rivera_Michael_Expressions/js/script.js
+++ Rivera_Michael_Expressions/js/script.js
@@ -1,9 +1,7 @@
 /**
  * Created by michaelrivera1 on 5/12/15.
  */
-// Concatenating Strings
-var areaCode = 407;
-var firstPart = 203;
-var secPart = 4529;
-var phoneNo = "("+ String(areaCode) +")" + String(firstPart) +"-" + String(secPart);
-console.log (phoneNo);
+var a = 3;
+//a ++;
+a--;
+console.log (a);
