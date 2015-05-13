b6669088ed23945414b14ef9a4cfcf5ee0a6ca8c
Michael-Rivera <mtrivera@fullsail.edu>
2015-05-13 03:34:21 -0600

    Added variables, title, and console log.
    Sparks age in human years wasn’t provided. I created a variable for his
    age and a variable for the result. The result variable is sparks age
    *7. I entered Sparkys age at 4 and the result that I got was twenty
    eight. I will next add descriptive tags to the output.

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -1,1 +1,7 @@
-alert("This is in the js file");
+// Michael Rivera Expressions Worksheet
+// How old is Sparky in Dog Years based on his actual age?
+
+var sparkysAge =4;
+var result = sparkysAge * 7;
+
+console.log (result);
