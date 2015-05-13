bcbf8cd11547a960f8d60cee41ad4938ebb15e5b
Michael-Rivera <mtrivera@fullsail.edu>
2015-05-13 04:31:27 -0600

    can't get it to work
    I’ll need to start a new js file for each problem

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -1,9 +1,26 @@
 // Michael Rivera Expressions Worksheet
 // How old is Sparky in Dog Years based on his actual age?
 
-var sparkysAge =4;  // Sparkys age in human years
-var dogYears =7;  // Sparkys age in dog years
-var result = sparkysAge * 7 ; // The result is Sparkys age in human years times dog years
+//var sparkysAge =4;  // Sparkys age in human years
+//var dogYears =7;  // Sparkys age in dog years
+//var result = sparkysAge * 7 ; // The result is Sparkys age in human years times dog years
 
-console.log ("Sparky is "+ sparkysAge + " human years old which is " + result + " in dog years.");
+//console.log ("Sparky is "+ sparkysAge + " human years old which is " + result + " in dog years.");
 // To get the console to print the desired result I had to add strings around the variables in the console log.
+
+// I commented out the previous problem so that it wouldn't interfere with the next problem
+
+// Slice of Pie part 1
+
+// How many slices can each person have?
+
+var slices = 8;
+
+var people = 19;
+
+var pizzas = 5;
+
+
+var numSlices = slices * pizzas;
+
+print.log (numSlices);
