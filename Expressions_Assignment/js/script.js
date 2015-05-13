184e3a61d6a8a9f5565e66a6d6560ccf5c25149a
Michael-Rivera <mtrivera@fullsail.edu>
2015-05-13 13:49:05 -0600

    setup
    setup a template for the assignment

---
+++ Expressions_Assignment/js/script.js
@@ -0,0 +1,24 @@
+// Michael Rivera 5/13/15 Expressions Assignment
+// need to use more than 1 expression --, ++, +=, -=, *=, /=
+// needs to be relevant to me
+// needs concantative scripts
+// needs user input and output to the user
+
+var = ;
+var = ;
+var = [];
+var ="";
+var = "";
+var = "";
+
+
+
+prompt ();
+prompt();
+prompt();
+
+alert ();
+
+console.log ();
+console.log ();
+console.log();
