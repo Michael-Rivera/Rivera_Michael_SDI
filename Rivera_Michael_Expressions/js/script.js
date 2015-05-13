fe9c3c59add6e80521cda22a209d787a1d083f27
Michael-Rivera <mtrivera@fullsail.edu>
2015-05-13 01:47:17 -0600

    Casting numbers as strings
    I cast numbers as rings to concatenate a phone number

--- Rivera_Michael_Expressions/js/script.js
+++ Rivera_Michael_Expressions/js/script.js
@@ -2,8 +2,8 @@
  * Created by michaelrivera1 on 5/12/15.
  */
 // Concatenating Strings
-
-var a = "6";
-var b = "7";
-var result = a + b;
-console.log (result);
+var areaCode = 407;
+var firstPart = 203;
+var secPart = 4529;
+var phoneNo = "("+ String(areaCode) +")" + String(firstPart) +"-" + String(secPart);
+console.log (phoneNo);
