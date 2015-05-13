9d1413a3034e2fc3c71bf50f3e9cf3aee8bd981a
Michael-Rivera <mtrivera@fullsail.edu>
2015-05-13 02:51:24 -0600

    Used Prompts
    Used user input through prompts to calculate the area of a rectangle.

--- Rivera_Michael_Prompts_Alerts/js/script.js
+++ Rivera_Michael_Prompts_Alerts/js/script.js
@@ -1,1 +1,5 @@
-alert("This is in the js file");
+
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width.");
+var height = prompt("Please enter the height.");
+var area = width* height
+console.log(area);
