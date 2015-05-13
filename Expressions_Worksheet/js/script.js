211fbca9c12dc1a252ffd01d34df3c4d916642ae
Michael-Rivera <mtrivera@fullsail.edu>
2015-05-13 03:38:59 -0600

    Added descriptive text to the output
    Added descriptive text to the output

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -2,6 +2,6 @@
 // How old is Sparky in Dog Years based on his actual age?
 
 var sparkysAge =4;
-var result = sparkysAge * 7;
+var result = "Sparky is " + sparkysAge * 7 + " in dog years";
 
 console.log (result);
