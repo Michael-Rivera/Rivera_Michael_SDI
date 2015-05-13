5a1b7459115313091a2f6a829c93a31ecf6586c2
Michael-Rivera <mtrivera@fullsail.edu>
2015-05-13 00:56:51 -0600

    Used Arrays in Expressions
    Used Arrays in Expressions

--- Rivera_Michael_Expressions/js/script.js
+++ Rivera_Michael_Expressions/js/script.js
@@ -1,16 +1,8 @@
 /**
  * Created by michaelrivera1 on 5/12/15.
  */
-// Order of Operations
+// Using Arrays in Expressions
+var orangeBins = [234, 567, 883];
+var total = orangeBins [0] + orangeBins [1] + orangeBins [2];
 
-var Quiz1 = 87;
-var Quiz2 = 100;
-var Quiz3 = 60;
-var Quiz4 = 80;
- // The average is the sum of the items divided by the number of items
-var average = (Quiz1+Quiz2+Quiz3+Quiz4)/4;
-//console.log (average);
-var length = 7;
-var width = 6;
-var perimiter = length * 2 + width * 2;
-console.log(perimiter);
+console.log(total);
