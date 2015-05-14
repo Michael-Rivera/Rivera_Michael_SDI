7bc3aa1c8453d51d7e365d2f51ce901cf4cb8a58
Michael-Rivera <mtrivera@fullsail.edu>
2015-05-14 14:05:40 -0600

    Failed to troubleshoot
    ran out of time

--- Expressions_Assignment/js/script.js
+++ Expressions_Assignment/js/script.js
@@ -11,18 +11,26 @@ Expressions Assignment
 */
 alert ("Hi, I would like to know \nyour average lap time.");
 alert ("Please follow the prompts and only provide \nminutes or seconds when asked.");
-var lapMin = [0,0,0,];
-var lapSec = [0,0,0,];
-var ttlMin = (lapMin[3]+lapMin[4]+lapMin[5]);
-var ttlSec = (lapSec [3]+lapSec[4]+lapSec[5]);
-var avgMin = (ttlMin/3);
-var avgSec=  (ttlSec/3);
-prompt = ("How many minutes did it take to complete your first lap?");
-prompt = ("and how many seconds?");
-alert ("Thank you");
-prompt = ("Now how many minutes did it take you to \ncomplete your second lap?");
-prompt = ("and how many seconds");
-prompt = ("and finally. How many minutes did it take you to finish the third lap?");
-prompt = ("and how many seconds?");
-alert ("Your average laptime is"+ avgMin + "and" + avgSec+ " seconds.")
-console.log (lapTime[0]);
+var lapMin = [0,0,0];// I placed zeros in the array as not to affect the outcome but still make it function
+var lapSec = [0,0,0];
+var ttlMin = (lapMin[3]+lapMin[4]+lapMin[5]);//boxes 0,1,and 2 are zeros. 3,4,and 5 are user generated.
+var ttlSec = (lapSec [3]+lapSec[4]+lapSec[5]);// Identicle to previous array
+var avgMin = Number (ttlMin/3); // I think this is my problem but the console only tells me NaN. I can't explain.
+var avgSec = Number (ttlSec/3);// Same problem as before
+lapMin [3] = Number (prompt  ("How many minutes did it take to complete your first lap?"));
+lapSec [3] = Number (prompt  ("and how many seconds?"));
+alert                 ("Thank you");
+lapMin [4] = Number (prompt  ("Now how many minutes did it take you to \ncomplete your second lap?"));
+lapSec [4] = Number (prompt  ("and how many seconds"));
+lapMin [5] = Number (prompt  ("and finally. How many minutes did it take you to finish the third lap?"));
+lapSec [5] = Number (prompt  ("and how many seconds?"));
+// All prompts work as intended
+alert           ("Your average lap time is"+ avgMin + "and" + avgSec+ " seconds.");
+// the alert works except the two variables within are not numbers and I don't know why.
+// In my testing I used 9 min 9 sec, 8 min 8 sec, and 7 min 7sec. The average is 8 and easy to troubleshoot.
+// Ran out of time. I haven't slept but 3 hours in 3 days and my brain is fried.
+console.log (ttlMin);
+/*console.log (lapSec);
+console.log (ttlMin);
+console.log (avgMin);
+console.log (avgSec);*/
