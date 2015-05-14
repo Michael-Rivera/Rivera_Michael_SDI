17615240f002fe95fa82e20315ae9fd23ee3e095
Michael-Rivera <mtrivera@fullsail.edu>
2015-05-13 19:09:48 -0600

    Build basic idea
    I wrote the prompts and alerts that I want. I am just having trouble
    remembering how to add user input to arrays to get my variables. This
    is a big problem

--- Expressions_Assignment/js/script.js
+++ Expressions_Assignment/js/script.js
@@ -1,24 +1,27 @@
-// Michael Rivera 5/13/15 Expressions Assignment
-// need to use more than 1 expression --, ++, +=, -=, *=, /=
-// needs to be relevant to me
-// needs concantative scripts
-// needs user input and output to the user
+/* Michael Rivera
+5/13/15
+ section 01
+Expressions Assignment
+ need to use more than 1 expression --, ++, +=, -=, *=, /=
+ needs to be relevant to me
+ needs concantative scripts
+ needs user input and output to the user
 
-var = ;
-var = ;
-var = [];
-var ="";
-var = "";
-var = "";
-
-
-
-prompt ();
-prompt();
-prompt();
-
-alert ();
-
-console.log ();
-console.log ();
-console.log();
+ What is your average lap time?
+*/
+alert ("Hi, I would like to know \nyour average lap time.");
+alert ("Please follow the prompts and only provide \nminutes or seconds when asked.");
+var lapOne = prompt [];
+var ttlMin
+var ttlSec
+var avgMin = (ttlMin/3);
+var avgSec=  (ttlSec/3);
+prompt = ("How many minutes did it take to complete your first lap?");
+prompt = ("and how many seconds?");
+alert ("Thank you");
+prompt = ("Now how many minutes did it take you to \ncomplete your second lap?");
+prompt = ("and how many seconds");
+prompt = ("and finally. How many minutes did it take you to finish the third lap?");
+prompt = ("and how many seconds?");
+alert ("Your average laptime is"+ avgMin + "and" + avgSec+ " seconds.")
+console.log (lapTime[0]);
