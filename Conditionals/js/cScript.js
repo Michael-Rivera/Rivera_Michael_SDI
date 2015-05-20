08b15cf7639bceccec25d104e42769e2d0718ec0
Michael-Rivera <mtrivera@fullsail.edu>
2015-05-20 15:43:51 -0600

    Finished Screencasts
    Completed all of the screencasts

--- Conditionals/js/cScript.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by michaelrivera1 on 5/19/15.
- */
-
-var oldEnough = true;
-
---
+++ Conditionals/js/script.js
@@ -0,0 +1,43 @@
+/*conditioinals
+Michael Rivera
+5/20/2015
+ */
+
+/*var kidHeight =44;
+var minHeight =48;
+var wParentHeight =45;
+if (kidHeight>minHeight){
+    console.log("You can ride the coaster!");
+}else if(kidHeight>wParentHeight){
+    console.log ("You can ride but only with a parent present.");
+}else{
+    console.log ("Sorry kid you got some growing to do first!");
+};
+
+
+//console.log("What comes after?");*/
+
+/*var budget = 100;
+var iphone = 199.99;
+var wonLottery= true;
+
+if(iphone<budget||wonLottery){
+    console.log("We can buy the phone!");
+}else{
+    console.log ("No phone for you!!");
+}*/
+
+/*var gpa=48;
+if(gpa>2.0){
+    console.log("You can graduate!");
+}else{
+    console.log("GPA is to low.");
+}
+
+gpa>2.0 ? console.log("You can graduate!") : console.log("GPA is to low.");*/
+
+var age= 9;
+var book
+
+book = (age<10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log (book);
