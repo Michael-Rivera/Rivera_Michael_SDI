368038ff6713bfb5b1c9e86f77b0a540b267cd15
Michael-Rivera <mtrivera@fullsail.edu>
2015-05-20 19:07:23 -0600

    replaced files
    replaced the js and html files and got a successful test

---
+++ Rivera_Michael_Conditionals_Assignment/js/script.js
@@ -0,0 +1,9 @@
+/*
+Michael Rivera
+Section01
+5/20/2015
+Conditionals Assignment
+ */
+//I tried creating a directory within WebStorm with a js file and an html file
+// It didn't work so I copied files and pasted them within the assignment folder in Finder
+//alert ("This is just a test.");  The test was successful 
